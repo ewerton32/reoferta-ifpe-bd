1b3a8152e853a7164418f48f280596b4d0c853a2
Ewerton Souto <otavio.souto@gmail.com>
2018-06-17 12:40:41 -0300

    #05 - inclui metodos post, delete e update

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -13,10 +13,61 @@ router.get("/task", (req, res, next) => {
 
 router.get("/task:id", (req, res, next) => {
     //res.send("API AQUI");
-    db.tasks.findOne({_id: req.params._id},(err, task) => {
+    db.tasks.findOne({_id: mongojs.ObjectId(req.params._id)},(err, task) => {
         if(err) return next(err);
         res.json(task);
     });
 });
+
+router.post("/tasks", (req, res, next) => {
+    const task = req.body;
+    if(!task.title || !(task.isDone)+"") {
+          res.status(400).json({
+            error: "Bad data!"
+
+          });  
+
+    } else {
+        db.task.save(task, (err, task) => {
+            if(err) return next(err);
+            res.json(task);
+        });
+    }
+
+
+})
+//metodo delete para remover dados
+router.delete("/tasks:id", (req, res, next) => {
+    db.tasks.remove({_id: mongojs.ObjectId(req.params.id)}, (err, result) => {
+        if(err) return next(err);
+        res.json(result);
+    });    
+})
+
+router.put("/tasks:id", (req, res, next) => {
+    const task = req.body;
+    const updateTask = { };
+
+    if(task.isDone){
+        updateTask.isDone = task.isDone;
+    }
+
+    if( task.title){
+        updateTask.title = task.title;
+    }
+
+    if(!updateTask) {
+        res.status(400).json({
+            error : "Bad Request!"
+        });
+     } else {
+        
+        db.tasks.update({_id: mongojs.ObjectId(req.params.id)}, (err, task) =>{
+            if (err) return next(err);
+            res.json(task);
+        
+        });
+    };
+})
 
 module.exports = router;
